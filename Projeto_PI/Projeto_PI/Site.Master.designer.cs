1a9de1bb59d45e5c74ad642e405b8d4e39c282ba
rafael086 <32245986+rafael086@users.noreply.github.com>
2017-09-25 17:55:24 -0300

    instalação do bootstrap e jquery usando o nuget

---
+++ Projeto_PI/Projeto_PI/Site.Master.designer.cs
@@ -0,0 +1,24 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace Projeto_PI {
+    
+    
+    public partial class Site {
+        
+        /// <summary>
+        /// Controle head.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+    }
+}
