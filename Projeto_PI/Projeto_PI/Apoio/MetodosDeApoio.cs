f538d537e7fb3b407c7c6c1cf94990006b084ce5
rafael086 <32245986+rafael086@users.noreply.github.com>
2017-10-01 21:10:49 -0300

    Classe de apoio
    Utilizada para criacao de metodos de extensão

--- Projeto_PI/Projeto_PI/Apoio/MetodosDeApoio.cs
+++ Projeto_PI/Projeto_PI/Apoio/MetodosDeApoio.cs
@@ -6,8 +6,16 @@ using System.Web;
 
 namespace Projeto_PI.Apoio
 {
+    /// <summary>
+    /// Classe criada para criacao de metodos de extensão
+    /// </summary>
     public static class MetodosDeApoio
     {
+        /// <summary>
+        /// Retira a mascara dos valores que vem da mascara dos formularios
+        /// </summary>
+        /// <param name="input">string que vai ter a mascara retirada</param>
+        /// <returns>string sem a mascara</returns>
         public static string TiraMascara(this string input)
         {
             return Regex.Replace(input, "[^a-zA-z0-9]","");
